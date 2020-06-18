e589747551ea60111b76610df4564e00d357912c
Jacob <puxin.xu@gmail.com>
2020-06-17 21:30:00 -0400

    fix: changes

--- squirrelfriends/squirrel/_grid_search.py
+++ squirrelfriends/squirrel/_grid_search.py
@@ -2,13 +2,9 @@ import itertools
 import logging
 
 import numpy as np
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, GroupKFold
 
 from ._lightgbm import lgbSquirrel
-
-logging.basicConfig(format="%(asctime)s %(message)s",
-                    datefmt="%m/%d/%Y %I:%M:%S %p",
-                    level=logging.INFO)
 
 
 def _parse_param_grid(grid):
@@ -30,21 +26,24 @@ def _parse_param_grid(grid):
     return res
 
 
-def search_cv(model, grid, X, y, eval_func=None, n_splits=4, seed=2020):
+def search_cv(model, grid, X, y, eval_func=None, n_splits=4, seed=2020, fold_type="KFold", groups=None):
     """Grid search.
 
     Args:
-        model (:obj:): model to search hyper-parameter.
-        grid (dict): (param name: list of value).
+        model (object): model to search hyper-parameter.
+        grid (dict): {param name: list of value}.
         X (DataFrame): training data.
         y (DataFrame): test data.
         eval_func (func): callable function in evaluation,
             eval_func(y_true, y_pred).
         n_splits (int): number of splits in Kflod().
         seed (int): random seed in Kfold().
+        fold_type (str): type of kfold algorithm, KFold or GroupKFold.
+        groups (array): group labels for the samples used
+            while splitting the dataset into train/test set.
 
     Returns:
-        search_results (dict): result og grid search,
+        search_results (list): result og grid search,
             - parameter (dict): param dictionary,
             - cv_results (dict): dict of cv results,
             - eval_score (float) mean of evalation score.
@@ -58,23 +57,25 @@ def search_cv(model, grid, X, y, eval_func=None, n_splits=4, seed=2020):
 
         # Search cv on lightgbm
         if hasattr(model, "__model__") and model.__model__ == "lgbSquirrel":
-            model.eval_func = eval_func
-            if "num_iterations" in param.keys():
-                model.build_lgb(param)
-            else:
-                model.build_sklgb(param)
+            model.eval_func = eval_func if eval_func else model.eval_func
+            model.build_lgb(param)
             cv_results = model.kfold_cv_lgb_train(
-                X=X, y=y, n_splits=n_splits, seed=seed)
+                X=X, y=y, n_splits=n_splits, seed=seed,
+                fold_type=fold_type, groups=groups)
 
         # Search cv on other sklearn model
         else:
             cv_model = model(**param)
 
-            kfold = KFold(n_splits=n_splits, shuffle=True, random_state=seed)
+            if fold_type == "KFold":
+                kfold = KFold(n_splits=n_splits,
+                              shuffle=True, random_state=seed)
+            elif fold_type == "GroupKFold":
+                kfold = GroupKFold(n_splits=n_splits)
 
             cv_results = []
 
-            for fold, (ind_train, ind_test) in enumerate(kfold.split(X, y)):
+            for fold, (ind_train, ind_test) in enumerate(kfold.split(X, y, groups)):
                 logging.info("cv: %d out of %d" % (fold + 1, n_splits))
 
                 X_train, X_test = X.loc[ind_train, :], X.loc[ind_test, :]
