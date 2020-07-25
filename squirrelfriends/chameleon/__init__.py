4f49ada4aa25200ae8daa52480528739211a8fe9
Jacob <puxin.xu@gmail.com>
2020-07-25 10:42:05 -0400

    restart

--- squirrelfriends/chameleon/__init__.py
+++ squirrelfriends/chameleon/__init__.py
@@ -1,3 +1,9 @@
 from ._visualize import (get_image_list,
                          draw_rectangles,
                          imgs_plot)
+
+__all__ = [
+    "get_image_list",
+    "draw_rectangles",
+    "imgs_plot"
+]
