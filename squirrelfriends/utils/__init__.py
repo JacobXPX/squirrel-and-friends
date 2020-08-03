a7d64f6b5b6b07ce9e76c26a40bcba8ef0c3cd8c
Jacob <puxin.xu@gmail.com>
2020-08-02 21:31:05 -0400

    fix init

--- squirrelfriends/utils/__init__.py
+++ squirrelfriends/utils/__init__.py
@@ -1,7 +1,8 @@
 from .validation import check_consistent_length
-from.storage import size_of_df
+from.storage import size_of_df, reduce_memory_usage
 
 __all__ = [
     "check_consistent_length",
-    "size_of_df"
+    "size_of_df",
+    "reduce_memory_usage"
 ]
