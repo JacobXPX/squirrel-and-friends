54b8361fed7cb9d45f914b8d789e7d01b620dbb4
Jacob <puxin.xu@gmail.com>
2020-08-08 22:29:31 -0400

    fix init

--- squirrelfriends/chameleon/__init__.py
+++ squirrelfriends/chameleon/__init__.py
@@ -1,10 +1,10 @@
 from ._torch_augment import (get_test_transforms, get_train_transforms,
                              get_transforms, get_valid_transforms)
 from ._torch_dataset import datasetRetriever
-from ._torch_tta import (get_tta_transformers, horizontalFlip, rotate90, ttaCompose,
-                         verticalFlip)
-from ._visualize import draw_rectangles, get_image_list, imgs_plot
-
+from ._torch_tta import (get_tta_transformers, horizontalFlip, rotate90,
+                         ttaCompose, verticalFlip)
+from ._visualize import (draw_rectangles, get_image_list, imgs_file_plot,
+                         imgs_plot)
 
 __all__ = [
     "get_image_list",
